39706148aad890eebac853923ff545e21e0ec249
iggallardo <iggallardo@uba.ar>
2024-10-18 18:32:10 -0300

    Taller 4 hasta iteradores

--- Talleres/taller4/src/main/java/aed/ABB.java
+++ Talleres/taller4/src/main/java/aed/ABB.java
@@ -11,57 +11,155 @@ public class ABB<T extends Comparable<T>> implements Conjunto<T> {
         T valor;
         Nodo izquierdo;
         Nodo derecho;
+        Nodo padre;
 
-        public Nodo() {
-            this.valor = null; 
-            this.siguiente = null;
-            this.anterior = null;
+        public Nodo(T valor, Nodo padre) {
+            this.valor = valor; 
+            this.izquierdo = null;
+            this.derecho = null;
+            this.padre = padre;
         }
     }
 
     public ABB() {
-        root = new Nodo();
+        root = null;
     }
 
     public int cardinal() {
-        throw new UnsupportedOperationException("No implementada aun");
+        return cardinalNodo(root);
+    }
+
+    private int cardinalNodo(Nodo root){
+        if(root == null) return 0;
+        return 1 + cardinalNodo(root.izquierdo) + cardinalNodo(root.derecho);
+
     }
 
     public T minimo(){
-        throw new UnsupportedOperationException("No implementada aun");
+        return minimoNodo(root);
+    }
+
+    private T minimoNodo(Nodo root){
+        if (root.izquierdo == null) return root.valor;
+        
+        return minimoNodo(root.izquierdo);
     }
 
     public T maximo(){
-        throw new UnsupportedOperationException("No implementada aun");
+        return maximoNodo(root);
+    }
+
+    private T maximoNodo(Nodo root){
+        if (root.derecho == null) return root.valor;
+        
+        return maximoNodo(root.derecho);
     }
 
     public void insertar(T elem){
-        if (root.valor == null) root.valor = elem;
+        root = insertNodo(elem, root, null);
+    }
+
+    private Nodo insertNodo(T elem, Nodo root, Nodo padre){
+        if(root == null) return new Nodo(elem, padre);
+
+        int comparacion = root.valor.compareTo(elem);
+
+        if (comparacion > 0) root.izquierdo = insertNodo(elem, root.izquierdo, root);
+        if (comparacion < 0) root.derecho = insertNodo(elem, root.derecho, root);
+
+        return root;
     }
 
     public boolean pertenece(T elem){
-        if (root.valor == elem) return true;
-
-        return false;
+        return perteneceNodo(elem, root);
     }
 
-    public void eliminar(T elem){
-        throw new UnsupportedOperationException("No implementada aun");
+    private boolean perteneceNodo(T elem, Nodo root){
+        if(root == null) return false;
+
+        int comparacion = root.valor.compareTo(elem);
+
+        if (comparacion > 0) return perteneceNodo(elem, root.izquierdo);
+        if (comparacion < 0) return perteneceNodo(elem, root.derecho);
+    
+        return true;
+    }
+
+    public void eliminar (T elem){
+        root = eliminarNodo(elem, root);
+    }
+
+    private Nodo eliminarNodo(T elem, Nodo root){
+        if (root == null) return root;
+        
+        if (root.valor != elem){
+            int comparacion = root.valor.compareTo(elem);
+
+            if (comparacion > 0) root.izquierdo = eliminarNodo(elem, root.izquierdo);
+            if (comparacion < 0) root.derecho = eliminarNodo(elem, root.derecho);
+        
+        } else{
+            if(root.derecho == null && root.izquierdo == null) return null;
+            //Cuidado estos casos y sus padres
+            else if(root.derecho == null && root.izquierdo != null) {
+                root.izquierdo.padre = root.padre;
+
+                return root.izquierdo;
+            }else if(root.derecho != null && root.izquierdo == null) {
+                root.derecho.padre = root.padre;
+                
+                return root.derecho;
+            }else if(root.derecho != null && root.izquierdo != null){
+                T maximo = maximoNodo(root.izquierdo);
+
+                root.valor = maximo;
+
+                root.izquierdo = eliminarNodo(maximo, root.izquierdo);
+            }
+        }
+
+        return root;
     }
 
     public String toString(){
         throw new UnsupportedOperationException("No implementada aun");
     }
 
-    private class ABB_Iterador implements Iterador<T> {
-        private Nodo _actual;
+    //Minimo Nodo 
+    private Nodo minimoNodoIt(Nodo root){
+        if (root.izquierdo == null) return root;
+        
+        return minimoNodoIt(root.izquierdo);
+    }
 
-        public boolean haySiguiente() {            
-            throw new UnsupportedOperationException("No implementada aun");
+    private class ABB_Iterador implements Iterador<T> {
+        private Nodo nodoActual;
+        private T valorActual;
+
+        public ABB_Iterador(){
+            nodoActual = minimoNodoIt(root);
+            valorActual = root.valor;
+        }
+
+        public boolean haySiguiente() {  
+            valorActual = nodoActual.valor;
+            
+            if(nodoActual.derecho != null){
+                return true;
+            }
+            if(nodoActual.padre != null){
+                nodoActual = nodoActual.padre;
+
+                if(valorActual.compareTo(nodoActual.valor) > 1) return true;
+            }
+
+            return false;
         }
     
         public T siguiente() {
-            throw new UnsupportedOperationException("No implementada aun");
+            haySiguiente();
+
+            return valorActual;
         }
     }
 
